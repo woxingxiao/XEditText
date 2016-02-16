02bd5dc24dee24e4ee0d136c9b4c784c00eb4632
woxingxiao <185578499@qq.com>
2016-02-16 15:38:37 +0800

    fixed the place of PopupWindow showing

--- sample/src/main/java/com/xw/sample/xedittext/MarkerPopWindow.java
+++ sample/src/main/java/com/xw/sample/xedittext/MarkerPopWindow.java
@@ -24,7 +24,7 @@ public class MarkerPopWindow extends PopupWindow {
         this.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
         this.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
         this.setAnimationStyle(R.style.Anim);
-        this.showAtLocation(rootLayout, Gravity.BOTTOM, x , y - rootLayout.getHeight());
+        this.showAtLocation(rootLayout, Gravity.NO_GRAVITY, x, y - rootLayout.getHeight() - 100); // adjust a little bit
     }
 
 }
